f1bf0cfb8318e2b11a5cf19221a906bd7fee1521
§ <1309346392@qq.com>
2023-01-12 11:30:21 +0800

    Finish SiteMeta Customize

--- data/siteMetadata.js
+++ data/siteMetadata.js
@@ -2,24 +2,24 @@
 
 /** @type {import("pliny/config").PlinyConfig } */
 const siteMetadata = {
-  title: 'Next.js Starter Blog',
-  author: 'Tails Azimuth',
-  headerTitle: 'TailwindBlog',
-  description: 'A blog created with Next.js and Tailwind.css',
-  language: 'en-us',
+  title: 'Jack Fan - Personal Site',
+  author: 'Jack Fan',
+  headerTitle: 'Jack Fan',
+  description: 'Jack Fan Personal Site  created with Next.js and Tailwind.css.',
+  language: 'zh-cn',
   theme: 'system', // system, dark or light
   siteUrl: 'https://tailwind-nextjs-starter-blog.vercel.app',
   siteRepo: 'https://github.com/timlrx/tailwind-nextjs-starter-blog',
-  siteLogo: '/static/images/logo.png',
-  image: '/static/images/avatar.png',
-  socialBanner: '/static/images/twitter-card.png',
-  email: 'address@yoursite.com',
-  github: 'https://github.com',
-  twitter: 'https://twitter.com/Twitter',
-  facebook: 'https://facebook.com',
-  youtube: 'https://youtube.com',
-  linkedin: 'https://www.linkedin.com',
-  locale: 'en-US',
+  siteLogo: '/static/images/logo.webp',
+  image: '/static/images/logo.webp',
+  socialBanner: '/static/images/logo.webp',
+  email: '1309346392@qq.com',
+  github: 'https://github.com/FM3676',
+  twitter: '',
+  facebook: '',
+  youtube: '',
+  linkedin: '',
+  locale: 'zh-CN',
   analytics: {
     // If you want to use an analytics provider you have to add it to the
     // content security policy in the `next.config.js` file.
