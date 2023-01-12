5cae9361d857388c4896aa668e6407eae38b71ac
§ <1309346392@qq.com>
2023-01-12 16:07:21 +0800

    Change Tag Style

--- components/Tag.tsx
+++ components/Tag.tsx
@@ -9,7 +9,9 @@ const Tag = ({ text }: Props) => {
   return (
     <Link
       href={`/tags/${kebabCase(text)}`}
-      className="mr-3 text-sm font-medium uppercase text-primary-500 hover:text-primary-600 dark:hover:text-primary-400"
+      // className="mr-3 text-sm font-medium uppercase text-primary-500 hover:text-primary-600 dark:hover:text-primary-400"
+      className="mr-3 text-sm font-medium uppercase border-blue-200 text-blue-300 hover:bg-blue-400
+      hover:text-white border p-1 rounded-md overflow-hidden transition-all"
     >
       {text.split(' ').join('-')}
     </Link>
